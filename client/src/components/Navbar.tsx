4076ce31c206bffbd007ca39c667a834e40e0216
henil88 <henilrathod88@gmail.com>
2025-10-25 13:32:50 +0530

    style:upadated home style

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center px-12 py-4">
       <div className="Logo">
         <div className="Icon"></div>
-        <span className="text-4xl font-bitcount">Talkaro</span>
+        <span className="text-4xl font-bitcount cursor-pointer">Talkaro</span>
       </div>
       <span className="w-0"></span>
     </div>
