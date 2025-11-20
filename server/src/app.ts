471d01aa299484d29b015fa4eb66b6caa25a8aa8
henil88 <henilrathod88@gmail.com>
2025-11-20 21:56:45 +0530

    feat(server): add MongoDB integration with Mongoose (DB connect + user model)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import router from "./routes/routes";
+import connectDB from "./database";
+
 const app = express();
 
+connectDB();
 app.use(express.json());
 app.use(router);
+
+
 app.get("/", (req, res) => {
   res.send("htttp server rinning perfect");
 });
